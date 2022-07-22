444e814005bcaad86be588adab2e888ef5dd97c8
soochan <tncksdl05@gmail.com>
2022-07-22 23:30:41 +0900

    Add : add initial value
    add initial value

--- src/main/java/server/jwt/example/Application.java
+++ src/main/java/server/jwt/example/Application.java
@@ -1,7 +1,16 @@
 package server.jwt.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import server.jwt.example.domain.AppUser;
+import server.jwt.example.domain.Role;
+import server.jwt.example.service.UserService;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +19,38 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+
+
+	// run after the application has initialized
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role("ROLE_USER"));
+			userService.saveRole(new Role("ROLE_MANAGER"));
+			userService.saveRole(new Role("ROLE_ADMIN"));
+			userService.saveRole(new Role("ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new AppUser("aaaaa", "aaaaa", "12345", new ArrayList<>()));
+			userService.saveUser(new AppUser("bbbbb", "bbbbb", "12345", new ArrayList<>()));
+			userService.saveUser(new AppUser("ccccc", "ccccc", "12345", new ArrayList<>()));
+			userService.saveUser(new AppUser("ddddd", "ddddd", "12345", new ArrayList<>()));
+
+			userService.addRoleToUser("aaaaa", "ROLE_USER");
+			userService.addRoleToUser("aaaaa", "ROLE_MANAGER");
+
+
+			userService.addRoleToUser("bbbbb", "ROLE_MANAGER");
+			userService.addRoleToUser("ccccc", "ROLE_ADMIN");
+
+			userService.addRoleToUser("ddddd", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("ddddd", "ROLE_ADMIN");
+			userService.addRoleToUser("ddddd", "ROLE_USER");
+		};
+	}
 }
